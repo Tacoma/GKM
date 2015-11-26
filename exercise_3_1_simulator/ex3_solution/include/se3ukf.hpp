6c3acbe695ea1e8e5c429ccd602ab254d1e822ef
Benjamin Grzimek <bengrzimek@online.de>
2015-11-26 20:20:54 +0100

    comments and small changes
    - added "_" to all member variables
    - added comments and links to paper

--- exercise_3_1_simulator/ex3_solution/include/se3ukf.hpp
+++ exercise_3_1_simulator/ex3_solution/include/se3ukf.hpp
@@ -37,25 +37,25 @@ private:
 	typedef std::vector<SE3Type, Eigen::aligned_allocator<SE3Type> > SE3TypeVector;
 	typedef std::vector<Vector3, Eigen::aligned_allocator<Vector3> > Vector3Vector;
 
-	SE3Type pose;
-	Vector3 linear_velocity;
-	Vector3 accel_bias;
-	Vector3 gyro_bias;
+	SE3Type pose_;
+	Vector3 linear_velocity_;
+	Vector3 accel_bias_;
+	Vector3 gyro_bias_;
 
-	Matrix15 covariance;
+	Matrix15 covariance_;
 
-	SE3TypeVector sigma_pose;
-	Vector3Vector sigma_linear_velocity;
-	Vector3Vector sigma_accel_bias;
-	Vector3Vector sigma_gyro_bias;
+	SE3TypeVector sigma_pose_;
+	Vector3Vector sigma_linear_velocity_;
+	Vector3Vector sigma_accel_bias_;
+	Vector3Vector sigma_gyro_bias_;
 
-	unsigned int iteration;
+	unsigned int iteration_;
 
 	// The function verifies that the angle increment is sufficiently small not to hit singularity.
 	// If the error occures that means the filter has diverged.
 	void print_if_too_large(const Vector3 & update, const std::string & name) {
 		if (update.array().abs().maxCoeff() > M_PI / 4) {
-			std::cout << "[" << iteration << "]" << name
+			std::cout << "[" << iteration_ << "]" << name
 					<< " has too large elements " << std::endl
 					<< update.transpose() << std::endl;
 			exit(-1);
@@ -63,46 +63,70 @@ private:
 
 	}
 
-	// Compute sigma points from covariance matrix
+	/** 
+ 	 * Compute sigma points from covariance matrix (line 52 and 53 in http://arxiv.org/pdf/1107.1119.pdf p.12)
+	 * with g(u_t,sigma_old) = sigma_old + u_t // u_t = delta ?
+	 * @param delta _delta_ of measurement, odometry and bias // local vector-space view ?
+	 *   delta.template head<6>: measurement in SE3
+	 *   delta.template segment<3>(6): linear velocity
+	 *   delta.template segment<3>(9/12): gyro_bias_ and gyro_bias_
+	 *   (it is probably only needed for line 64 in http://arxiv.org/pdf/1107.1119.pdf p.12)
+	 * @param covariance_
+	 * @param pose_
+	 * @param linear_velocity
+	 * @param accel_bias_
+	 * @param gyro_bias_
+	 *
+	 * @return sigma_pose_
+	 * @return sigma_linear_velocity_
+	 * @return sigma_accel_bias_
+	 * @return sigma_gyro_bias_
+	*/
 	void compute_sigma_points(const Vector15 & delta = Vector15::Zero()) {
 		print_if_too_large(delta.template segment<3>(3), "Delta");
 
-		Eigen::LLT<Matrix15> llt_of_covariance(covariance);
+		// computes Cholesky decomposition, result is a lower left triangle (llt) matrix L 
+		// with L * L^T = original matrix
+		Eigen::LLT<Matrix15> llt_of_covariance(covariance_);
 		assert(llt_of_covariance.info() == Eigen::Success);
 		Matrix15 L = llt_of_covariance.matrixL();
 
-		sigma_pose[0] = pose * SE3Type::exp(delta.template head<6>());
-        sigma_linear_velocity[0] = linear_velocity + delta.template segment<3>(6);
-		sigma_accel_bias[0] = accel_bias + delta.template segment<3>(9);
-		sigma_gyro_bias[0] = gyro_bias + delta.template segment<3>(12);
+		// sigma(0)_old = mean
+		// sigma(0)_new = sigma(0)_old + delta ( equals g(u_t, sigma_old) in line 53)
+		sigma_pose_[0] = pose_ * SE3Type::exp(delta.template head<6>());
+        	sigma_linear_velocity_[0] = linear_velocity_ + delta.template segment<3>(6);
+		sigma_accel_bias_[0] = accel_bias_ + delta.template segment<3>(9);
+		sigma_gyro_bias_[0] = gyro_bias_ + delta.template segment<3>(12);
 
 		for (int i = 0; i < STATE_SIZE; i++) {
 			Vector15 eps = L.col(i);
-
-			sigma_pose[1 + i] = pose
+			
+			// sigma[1-15]_new = mean_old + sqrt(covariance_)[i] + delta // sqrt(covariance_)[i] = L.col(i)
+			sigma_pose_[1 + i] = pose_
 					* SE3Type::exp(
 							delta.template head<6>() + eps.template head<6>());
-
-			sigma_linear_velocity[1 + i] = linear_velocity
+			
+			sigma_linear_velocity_[1 + i] = linear_velocity_
 					+ delta.template segment<3>(6) + eps.template segment<3>(6);
 
-			sigma_accel_bias[1 + i] = accel_bias + delta.template segment<3>(9)
+			sigma_accel_bias_[1 + i] = accel_bias_ + delta.template segment<3>(9)
 					+ eps.template segment<3>(9);
 
-			sigma_gyro_bias[1 + i] = gyro_bias + delta.template segment<3>(12)
+			sigma_gyro_bias_[1 + i] = gyro_bias_ + delta.template segment<3>(12)
 					+ eps.template segment<3>(12);
-
-			sigma_pose[1 + STATE_SIZE + i] = pose
+			
+			// sigma[16-30]_new = mean_old - sqrt(covariance_)[i] + delta // sqrt(covariance_)[i] = L.col(i)
+			sigma_pose_[1 + STATE_SIZE + i] = pose_
 					* SE3Type::exp(
 							delta.template head<6>() - eps.template head<6>());
 
-			sigma_linear_velocity[1 + STATE_SIZE + i] = linear_velocity
+			sigma_linear_velocity_[1 + STATE_SIZE + i] = linear_velocity_
 					+ delta.template segment<3>(6) - eps.template segment<3>(6);
 
-			sigma_accel_bias[1 + STATE_SIZE + i] = accel_bias
+			sigma_accel_bias_[1 + STATE_SIZE + i] = accel_bias_
 					+ delta.template segment<3>(9) - eps.template segment<3>(9);
 
-			sigma_gyro_bias[1 + STATE_SIZE + i] = gyro_bias
+			sigma_gyro_bias_[1 + STATE_SIZE + i] = gyro_bias_
 					+ delta.template segment<3>(12)
 					- eps.template segment<3>(12);
 
@@ -116,8 +140,20 @@ private:
 		}
 	}
 
-	// Compute mean of the sigma points.
-	// For SE3 an iterative method should be used.
+	/**
+	 * Compute mean of the sigma points. (line 54 in http://arxiv.org/pdf/1107.1119.pdf p.12)
+	 * details not important ?
+	 * For SE3 an iterative method should be used.
+	 * @param sigma_pose_
+	 * @param sigma_linear_velocity_
+	 * @param sigma_accel_bias_
+	 * @param sigma_gyro_bias_
+	 * 
+	 * @return mean_pose
+	 * @return mean_linear_velocity
+	 * @return mean_accel_bias
+	 * @return mean_gyro_bias
+	 */
     void compute_mean(  SE3Type & mean_pose, Vector3 & mean_linear_velocity,
                         Vector3 & mean_accel_bias, Vector3 & mean_gyro_bias) {
 
@@ -125,15 +161,15 @@ private:
 		mean_accel_bias.setZero();
 		mean_gyro_bias.setZero();
 		for (int i = 0; i < NUM_SIGMA_POINTS; i++) {
-			mean_linear_velocity += sigma_linear_velocity[i];
-			mean_accel_bias += sigma_accel_bias[i];
-			mean_gyro_bias += sigma_gyro_bias[i];
+			mean_linear_velocity += sigma_linear_velocity_[i];
+			mean_accel_bias += sigma_accel_bias_[i];
+			mean_gyro_bias += sigma_gyro_bias_[i];
 		}
 		mean_linear_velocity /= NUM_SIGMA_POINTS;
 		mean_accel_bias /= NUM_SIGMA_POINTS;
 		mean_gyro_bias /= NUM_SIGMA_POINTS;
 
-		mean_pose = sigma_pose[0];
+		mean_pose = sigma_pose_[0];
 		Vector6 delta;
 
 		const static int max_iterations = 1000;
@@ -143,7 +179,7 @@ private:
 			delta.setZero();
 
 			for (int i = 0; i < NUM_SIGMA_POINTS; i++) {
-				delta += SE3Type::log(mean_pose.inverse() * sigma_pose[i]);
+				delta += SE3Type::log(mean_pose.inverse() * sigma_pose_[i]);
 			}
 			delta /= NUM_SIGMA_POINTS;
 
@@ -154,23 +190,37 @@ private:
 	}
 
 
-	// Compute mean and covariance from sigma points.
-	// For computing mean previous function can be used.
+	/**
+	 * Compute mean and covariance from sigma points. (line 54 and 55 in http://arxiv.org/pdf/1107.1119.pdf p.12)
+	 * straightforward sum of sigma points
+	 * For computing mean, previous function can be used.
+	 * @param sigma_pose_
+	 * @param sigma_linear_velocity_
+	 * @param sigma_accel_bias_
+	 * @param sigma_gyro_bias_
+	 * 
+	 * @return pose_
+	 * @return linear_velocity_
+	 * @return accel_bias_
+	 * @return gyro_bias_
+	 * 
+	 * @return covariance_
+	 */
     void compute_mean_and_covariance() {
-		compute_mean(pose, linear_velocity, accel_bias, gyro_bias);
-		covariance.setZero();
+		compute_mean(pose_, linear_velocity_, accel_bias_, gyro_bias_);
+		covariance_.setZero();
 
 		for (int i = 0; i < NUM_SIGMA_POINTS; i++) {
 			Vector15 eps;
 
-			eps.template head<6>() = SE3Type::log(pose.inverse() * sigma_pose[i]);
-			eps.template segment<3>(6) = sigma_linear_velocity[i] - linear_velocity;
-			eps.template segment<3>(9) = sigma_accel_bias[i] - accel_bias;
-			eps.template segment<3>(12) = sigma_gyro_bias[i] - gyro_bias;
+			eps.template head<6>() = SE3Type::log(pose_.inverse() * sigma_pose_[i]);
+			eps.template segment<3>(6) = sigma_linear_velocity_[i] - linear_velocity_;
+			eps.template segment<3>(9) = sigma_accel_bias_[i] - accel_bias_;
+			eps.template segment<3>(12) = sigma_gyro_bias_[i] - gyro_bias_;
 
-			covariance += eps * eps.transpose();
+			covariance_ += eps * eps.transpose();
 		}
-		covariance /= 2;
+		covariance_ /= 2;
 	}
 
 
@@ -183,55 +233,71 @@ public:
 			const Vector3 & initial_gyro_bias,
 			const Matrix15 & initial_covariance) {
 
-		sigma_pose.resize(NUM_SIGMA_POINTS);
-		sigma_linear_velocity.resize(NUM_SIGMA_POINTS);
-		sigma_accel_bias.resize(NUM_SIGMA_POINTS);
-		sigma_gyro_bias.resize(NUM_SIGMA_POINTS);
+		sigma_pose_.resize(NUM_SIGMA_POINTS);
+		sigma_linear_velocity_.resize(NUM_SIGMA_POINTS);
+		sigma_accel_bias_.resize(NUM_SIGMA_POINTS);
+		sigma_gyro_bias_.resize(NUM_SIGMA_POINTS);
 
-		iteration = 0;
-		pose = initial_pose;
-		linear_velocity = initial_linear_velocity;
-		accel_bias = initial_accel_bias;
-		gyro_bias = initial_gyro_bias;
-		covariance = initial_covariance;
+		iteration_ = 0;
+		pose_ = initial_pose;
+		linear_velocity_ = initial_linear_velocity;
+		accel_bias_ = initial_accel_bias;
+		gyro_bias_ = initial_gyro_bias;
+		covariance_ = initial_covariance;
 
 	}
 
 	SE3UKF(const SE3UKF & other) {
 
-		sigma_pose.resize(NUM_SIGMA_POINTS);
-		sigma_linear_velocity.resize(NUM_SIGMA_POINTS);
-		sigma_accel_bias.resize(NUM_SIGMA_POINTS);
-		sigma_gyro_bias.resize(NUM_SIGMA_POINTS);
+		sigma_pose_.resize(NUM_SIGMA_POINTS);
+		sigma_linear_velocity_.resize(NUM_SIGMA_POINTS);
+		sigma_accel_bias_.resize(NUM_SIGMA_POINTS);
+		sigma_gyro_bias_.resize(NUM_SIGMA_POINTS);
 
-		iteration = other.iteration;
-		pose = other.pose;
-		linear_velocity = other.linear_velocity;
-		accel_bias = other.accel_bias;
-		gyro_bias = other.gyro_bias;
-		covariance = other.covariance;
+		iteration_ = other.iteration_;
+		pose_ = other.pose_;
+		linear_velocity_ = other.linear_velocity_;
+		accel_bias_ = other.accel_bias_;
+		gyro_bias_ = other.gyro_bias_;
+		covariance_ = other.covariance_;
 
 	}
 
 	SE3UKF & operator=(const SE3UKF & other) {
 
-		sigma_pose.resize(NUM_SIGMA_POINTS);
-		sigma_linear_velocity.resize(NUM_SIGMA_POINTS);
-		sigma_accel_bias.resize(NUM_SIGMA_POINTS);
-		sigma_gyro_bias.resize(NUM_SIGMA_POINTS);
+		sigma_pose_.resize(NUM_SIGMA_POINTS);
+		sigma_linear_velocity_.resize(NUM_SIGMA_POINTS);
+		sigma_accel_bias_.resize(NUM_SIGMA_POINTS);
+		sigma_gyro_bias_.resize(NUM_SIGMA_POINTS);
 
-		iteration = other.iteration;
-		pose = other.pose;
-		linear_velocity = other.linear_velocity;
-		accel_bias = other.accel_bias;
-		gyro_bias = other.gyro_bias;
-		covariance = other.covariance;
+		iteration_ = other.iteration_;
+		pose_ = other.pose_;
+		linear_velocity_ = other.linear_velocity_;
+		accel_bias_ = other.accel_bias_;
+		gyro_bias_ = other.gyro_bias_;
+		covariance_ = other.covariance_;
 		return *this;
 	}
 
 
-    // TODO: exercise 2 b)
-	// Predict new state and covariance with IMU measurement.
+    	// TODO: exercise 2 b)
+	/**
+	 * Predict new state and covariance with IMU measurement.
+	 * @param accel_measurement
+	 * @param gyro_measurement
+	 * @param dt
+	 * @param accel_noice
+	 * @param gyro_noice
+	 * 
+	 * @param "old mean and covariance"
+	 * 
+	 * @return mean_pose 
+	 * @return mean_linear_velocity
+	 * @return mean_accel_bias
+	 * @return mean_gyro_bias
+	 * 	==> new mean
+	 * @return covariance_ new covariance
+	 */
     void predict(   const Vector3 & accel_measurement,
                     const Vector3 & gyro_measurement, 
 		    const _Scalar dt,
@@ -239,18 +305,20 @@ public:
 		    const Matrix3 & gyro_noise) {
 
         // 1. compute sigma points of current state
-        compute_sigma_points(); // add delta?
+        compute_sigma_points(); // DELETE: add delta? addendum: probably not, because the delta is also in line 64
 
         // 2. for each sigma point, apply given IMU motion model
+        // DELETE: isn't that done already in compute_sigma_points(delta), if we chose delta as our new measurement ?
+        // addendum: probably not, because the delta is also in line 64
         for(int i=0; i<NUM_SIGMA_POINTS; i++) {
             // get rotation and translation from the pose
-	    // Matrix3 rot = sigma_pose[i].topLeftCorner(3,3);
-	    // Vector3 t = sigma_pose[i].topRightCorner(3,1);
+	    // Matrix3 rot = sigma_pose_[i].topLeftCorner(3,3);
+	    // Vector3 t = sigma_pose_[i].topRightCorner(3,1);
 
-            sigma_pose[i].translation() = sigma_pose[i].translation() + ( sigma_linear_velocity[i] * dt );
-            sigma_linear_velocity[i]    = sigma_linear_velocity[i] + (sigma_pose[i].rotationMatrix() * (accel_measurement - sigma_accel_bias[i])) * dt;
-            sigma_pose[i].setRotationMatrix( sigma_pose[i].rotationMatrix() * 
-			Sophus::SO3Group<_Scalar>::exp( (gyro_measurement-sigma_gyro_bias[i])*dt ).matrix());
+            sigma_pose_[i].translation() = sigma_pose_[i].translation() + ( sigma_linear_velocity_[i] * dt );
+            sigma_linear_velocity_[i]    = sigma_linear_velocity_[i] + (sigma_pose_[i].rotationMatrix() * (accel_measurement - sigma_accel_bias_[i])) * dt;
+            sigma_pose_[i].setRotationMatrix( sigma_pose_[i].rotationMatrix() * 
+			Sophus::SO3Group<_Scalar>::exp( (gyro_measurement-sigma_gyro_bias_[i])*dt ).matrix());
         }
 
         // 3. compute new mean and covarience.
@@ -258,74 +326,100 @@ public:
     }
 
 
-    // TODO: exercise 2 c)
-	// Apply 6d pose measurement.
+    	// TODO: exercise 2 c)  (equals correction step in http://arxiv.org/pdf/1107.1119.pdf p.12)
+	// DELETE: compute line 56 with compute_sigma_points(delta) ? // if h(..., delta) is equal to g(..., 0) ?
+	// addendum: probably not because delta is also in line 64
+	// TODO: compute line 57
+	// DELETE: compute 58
+	// TODO: compute 60 with compute_mean_and_covariance() ?
+	// TODO: compute 59 analogue to compute_mean_and_covariance() ?
+	// TODO: compute 61, 62, 63 somehow
+	// DELETE: compute 64 with compute_sigma_points !! There is a delta in there !!
+	// DELETE: compute 65 and 66 with compute_mean_and_covariance() !
+	/**
+	 * Apply 6d pose measurement.
+	 * @param measured_pose
+	 * @param measurement_noise 6x6 covariance matrix ?
+	 * 
+	 * @return mean_pose 
+	 * @return mean_linear_velocity
+	 * @return mean_accel_bias
+	 * @return mean_gyro_bias
+	 * 	==> corrected mean
+	 * 
+	 * @return covariance_ corrected covariance
+	 */
 	void measurePose(const SE3Type & measured_pose,
 			const Matrix6 & measurement_noise) {
-	 
+		compute_sigma_points(); // 56
+		// 57
+		compute_mean_and_covariance(); // 58
+		// 59-63
+		compute_sigma_points(delta); // 64 TODO: delta
+		compute_mean_and_covariance(); // 65, 66
 	}
 
 	SE3Type get_pose() const {
-		return pose;
+		return pose_;
 	}
 
 	Vector3 get_linear_velocity() const {
-		return linear_velocity;
+		return linear_velocity_;
 	}
 
 	Matrix15 get_covariance() const {
-		return covariance;
+		return covariance_;
 	}
 
 	Vector3 get_accel_bias() const {
-		return accel_bias;
+		return accel_bias_;
 	}
 
 	Vector3 get_gyro_bias() const {
-		return gyro_bias;
+		return gyro_bias_;
 	}
 
-	// The function is a very basic test to check sigma points mean and covariance computation.
+	// The function is a very basic test to check sigma points mean and covariance_ computation.
 	// It computes sigma points from the current distribution and then computes the distribution from the sigma points.
 	// In the correct implementation two distributions should be identical up to numeric errors.
 	void test_sigma_points() {
-		SE3Type pose1 = pose;
-		Vector3 linear_velocity1 = linear_velocity;
-		Vector3 accel_bias1 = accel_bias;
-		Vector3 gyro_bias1 = gyro_bias;
-		Matrix15 covariance1 = covariance;
+		SE3Type pose1 = pose_;
+		Vector3 linear_velocity1 = linear_velocity_;
+		Vector3 accel_bias1 = accel_bias_;
+		Vector3 gyro_bias1 = gyro_bias_;
+		Matrix15 covariance1 = covariance_;
 
 		compute_sigma_points();
 		compute_mean_and_covariance();
 
-		SE3Type pose2 = pose;
-		Vector3 linear_velocity2 = linear_velocity;
-		Vector3 accel_bias2 = accel_bias;
-		Vector3 gyro_bias2 = gyro_bias;
-		Matrix15 covariance2 = covariance;
+		SE3Type pose2 = pose_;
+		Vector3 linear_velocity2 = linear_velocity_;
+		Vector3 accel_bias2 = accel_bias_;
+		Vector3 gyro_bias2 = gyro_bias_;
+		Matrix15 covariance2 = covariance_;
 
 		if ((covariance1 - covariance2).array().abs().maxCoeff() > 1e-5) {
-			std::cerr << "[" << iteration << "]" << "Covariance miscalculated\n"
+			std::cerr << "[" << iteration_ << "]" << "covariance_ miscalculated\n"
 					<< covariance1 << "\n\n" << covariance2 << "\n\n";
 			assert(false);
 		}
 
 		if ((linear_velocity1 - linear_velocity2).array().abs().maxCoeff()
 				> 1e-5) {
-			std::cerr << "[" << iteration << "]" << "Velocity miscalculated\n"
+			std::cerr << "[" << iteration_ << "]" << "Velocity miscalculated\n"
 					<< linear_velocity1 << "\n\n" << linear_velocity2 << "\n\n";
 			assert(false);
 		}
 
 		if ((accel_bias1 - accel_bias2).array().abs().maxCoeff() > 1e-5) {
-			std::cerr << "[" << iteration << "]"
+			std::cerr << "[" << iteration_ << "]"
 					<< "Accelerometer bias miscalculated\n" << accel_bias1
 					<< "\n\n" << accel_bias2 << "\n\n";
 			assert(false);
 		}
 
 		if ((gyro_bias1 - gyro_bias2).array().abs().maxCoeff() > 1e-5) {
-			std::cerr << "[" << iteration << "]"
+			std::cerr << "[" << iteration_ << "]"
 					<< "Gyroscope bias miscalculated\n" << gyro_bias1 << "\n\n"
 					<< gyro_bias2 << "\n\n";
 			assert(false);
